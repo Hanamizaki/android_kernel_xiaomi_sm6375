27968791b32fbf805dc1b4e93270aa2b1d56e8ce
Hanamizaki <164388739+Hanamizaki@users.noreply.github.com>
2024-03-25 02:29:17 +0530

    build_kernel: Adapt for Adrenaline Kernel

--- build_kernel.sh
+++ build_kernel.sh
@@ -20,6 +20,6 @@ KERNEL_CMDLINE="ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- O=out LLVM=1"
 #Zip the kernel    
 cp out/arch/arm64/boot/Image $(pwd)/AnyKernel3
 cp out/arch/arm64/boot/dts/vendor/qcom/blair.dtb $(pwd)/AnyKernel3
-cd AnyKernel3 && mv blair.dtb dtb && zip -r9 Rashoumon_veux_$(date +"%Y-%m-%d").zip *
+cd AnyKernel3 && mv blair.dtb dtb && zip -r9 Adrenaline_veux-1.0.zip *
 
-echo "Done!"
+echo "Succesfully Built!"
